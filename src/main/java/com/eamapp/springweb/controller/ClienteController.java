95087cfea3b9fa77e373de7b881bd264f9cfe62b
JohanMorales211 <morales.johan.8905@eam.edu.co>
2023-02-24 11:47:33 -0500

    creacion de venderores

--- src/main/java/com/eamapp/springweb/controller/ClienteController.java
+++ src/main/java/com/eamapp/springweb/controller/ClienteController.java
@@ -1,18 +1,35 @@
 package com.eamapp.springweb.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.eamapp.springweb.models.Cliente;
+import com.eamapp.springweb.models.Vendedores;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 
 @RequestMapping("/cliente")
 public class ClienteController {
-    @GetMapping({"/","","/cliente"})
+    @GetMapping({"/",""})
     public String indexCliente(Model model){
-        model.addAttribute("titulo", "Esta es la pagina es de clientes");
-        return "cliente/cliente";
+        Cliente cliente = new Cliente();
+        model.addAttribute("titulo", "Perfil");
+        model.addAttribute("cliente", cliente);
+        cliente.setNombre("Johan");
+        cliente.setApellido("Morales");
+        cliente.setEmail("morales.johan.8905@eam.edu.co");
+        model.addAttribute("cuerpo", "perfil del cliente: ".concat(cliente.getNombre()));
+        return "cliente/index";
+    }
+
+    @ModelAttribute("vendedores")
+    public List<Vendedores> poblarVendedores(){
+        List<Vendedores> vendedores = Arrays.asList(new Vendedores("Pepito","Perez","pepito@prueba.com"), new Vendedores("cosme","fulanito","cosme@prueba.com"));
+        return vendedores;
     }
 }
